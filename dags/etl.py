16da9898d0aa4afd90f4047ef7780cb20ee553b6
harshil0217 <harshil0217@gmail.com>
2024-08-16 14:00:09 -0400

    added extract from rss feed dag

--- dags/etl.py
+++ dags/etl.py
@@ -3,20 +3,26 @@ import sqlalchemy
 from airflow.decorators import dag, task
 import pendulum
 import feedparser
+import numpy as np
 
 #engine to connect to postgres
 #engine = sqlalchemy.create_engine('postgresql://airflow:airflow@postgres:5432')
 
 feed = 'https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100003114'
 
-@task()
-def extract():
+
+def extract(feed):
     feed = feedparser.parse(feed)
-    for entry in feed.entries:
-        print(entry.title)
-        print(entry.link)
-        print(entry.published)
-        print(entry.summary)
-        print('')
+    n = len(feed.entries)
+    #create empty dataframe
+    headlines = pd.DataFrame(np.nan, index = range(n), columns = ['Title', 'Link', 'Published'])
+    #iteratively fill headlines dataframe
+    for i in range(n):
+        headlines['Title'][i] = feed.entries[i].title
+        headlines['Link'][i] = feed.entries[i].link
+        headlines['Published'] = feed.entries[i].published
+    #export to csv
+    headlines.to_csv('headlines.csv')
+    
         
-extract()
+extract(feed)
